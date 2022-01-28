8be33e487cf08d09155e593d5e780094bfd6b2e6
razvanfulea <razvanfulea@gmail.com>
2022-01-29 01:01:46 +0200

    Couple more examples

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 import { Observable } from 'rxjs';
 
-console.log("hi");
-const observable = new Observable(subscriber => {
-	subscriber.next(1);
-	subscriber.next(2);
-	subscriber.next(3);
-	setTimeout(() => {
-		subscriber.next(4);
-		subscriber.complete();
-	}, 1000);
-});
+function first() {
+	const observable = new Observable(subscriber => {
+		subscriber.next(1);
+		subscriber.next(2);
+		subscriber.next(3);
+		setTimeout(() => {
+			subscriber.next(4);
+			subscriber.complete();
+		}, 1000);
+	});
+	
+	console.log('before subscribe');
+	observable.subscribe({
+		next(x) { console.log(`got ${x}`) },
+		error(err) { console.error(`error: ${err}`) },
+		complete() { console.log('complete') }
+	});
+	console.log('after subscribe');
+}
 
-console.log('before subscribe');
-observable.subscribe({
-	next(x) { console.log(`got ${x}`) },
-	error(err) { console.error(`error: ${err}`) },
-	complete() { console.log('complete') }
-});
-console.log('after subscribe');
+function emitEverySecond() {
+	const observable = new Observable(subscriber => {
+		setInterval(() => subscriber.next("hi"), 1000);
+	});
+	observable.subscribe(val => console.log(val));
+}
+
+function unsubscribeFromObservable() {
+	let i = 0;
+	const observable = new Observable(function subscribe(subscriber) {
+		const id = setInterval(() => subscriber.next(i++), 1000);
+		return function unsubscribe() { clearInterval(id); }
+	})
+	const subscription = observable.subscribe(val => console.log(val));
+	setTimeout(() => subscription.unsubscribe(), 6000);
+}
+
+const main = () => {
+	// first();
+	// emitEverySecond();
+	// unsubscribeFromObservable();
+}
+
+main();
